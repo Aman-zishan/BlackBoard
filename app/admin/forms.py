a940927e0752c1e220ac81cf7e2ad38bea8e6cb4
Aman-zishan <amanzishan.az@gmail.com>
2020-04-05 15:54:57 +0530

    added tasks and subjects

---
+++ app/admin/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class DepartmentForm(FlaskForm):
+    """
+    Form for admin to add or edit a department
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    
+class RoleForm(FlaskForm):
+    """
+    Form for admin to add or edit a role
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
